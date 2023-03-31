0b5f06b43702bae4a7f4628884c686682f9a6970
flacluque <flav64.lacluque@gmail.com>
2023-03-31 09:59:06 +0200

    ajout fonction edit et remove

--- src/Controller/CustomerController.php
+++ src/Controller/CustomerController.php
@@ -6,16 +6,18 @@ use App\Entity\Customer;
 use App\Repository\CustomerRepository;
 
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 class CustomerController extends AbstractController
 {
-    #[Route('/customers', name: 'customer_index')]
+    #[Route('/customers', name: 'customer_index', methods: "GET")]
     public function index(CustomerRepository $customerRepository): Response
     {
         $customers = $customerRepository->findAll();
@@ -25,13 +27,13 @@ class CustomerController extends AbstractController
         ]);
     }
 
-    #[Route('/customer/{id}', name: 'customer_show')]
+    #[Route('/customer/{id}', name: 'customer_show', methods: "GET")]
     public function show(CustomerRepository $customerRepository, int $id): Response
     {
         $customer = $customerRepository->find($id);
 
         if (!$customer) {
-            throw new NotFoundHttpException('Customer not found');
+            throw new NotFoundHttpException(sprintf('The customer with id %d does not exist', $id));
         }
 
         return $this->render('Customer/show.html.twig', [
@@ -63,7 +65,60 @@ class CustomerController extends AbstractController
 
         $customerRepository->save($customer);
 
-        $response = new JsonResponse(['message' => 'Customer added'], 201);
+        $response = new JsonResponse(['message' => sprintf('Le Client %s %s a bien été ajouté', $firstname, $lastname)], 201);
         return $response;
+    }
+
+    #[Route('/edit_customer/{id}', name: 'customer_edit', methods: "PUT")]
+    public function edit(CustomerRepository $customerRepository, Request $request, int $id): Response
+    {
+        try {
+            $data = json_decode($request->getContent(), true, 512, JSON_THROW_ON_ERROR);
+        } catch (\JsonException $e) {
+            throw new BadRequestHttpException('Invalid JSON format');
+        }
+
+        $customer = $customerRepository->find($id);
+
+        if (!$customer) {
+            throw new NotFoundHttpException(sprintf('The customer with id %d does not exist', $id));
+        }
+
+        $firstname = $data['firstname'] ?? null;
+        $lastname = $data['lastname'] ?? null;
+        $email = $data['email'] ?? null;
+        $phone = $data['phone'] ?? null;
+
+        if($firstname){
+            $customer->setFirstName($firstname);
+        }
+        if($lastname){
+            $customer->setLastName($lastname);
+        }
+        if($email){
+            $customer->setEmail($email);
+        }
+        if($phone){
+            $customer->setPhone($phone);
+        }
+
+
+        $customerRepository->save($customer);
+
+        return new JsonResponse(['message' => 'Le Client d\'id %d a bien été supprimé', $id]);
+    }
+
+    #[Route('/remove_customer/{id}', name: 'customer_remove', methods: "DELETE")]
+    public function remove(CustomerRepository $customerRepository, int $id, EntityManagerInterface $entityManager): Response
+    {
+        $customer = $customerRepository->find($id);
+
+        if (!$customer) {
+            throw new NotFoundHttpException(sprintf('The customer with id %d does not exist', $id));
+        }
+
+        $entityManager->remove($customer);
+        $entityManager->flush();
+        return new JsonResponse(['message' => sprintf('Le Client d\'id %d a bien été supprimé', $id)]);
     }
 }
